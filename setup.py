ae3f29819c360e30ef0f1a9873468a8016af93c8
Samuel Marks <samuelmarks@gmail.com>
2015-03-18 23:09:56 +1100

    .gitignore now brought into setup

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
-from python_package_gen import __version__, __author__
+from os import path
+from functools import partial
+from pip import get_installed_distributions, utils
+
 
 if __name__ == '__main__':
     package_name = 'python_package_gen'
+
+    templates_join = partial(path.join, path.dirname(__file__), package_name, 'templates')
+
+    location = path.sep + path.sep.join(path.dirname(utils.logging.__file__).split(path.sep)[1:-2])
+    # Still experimenting with: `get_installed_distributions(local_only=True)`
+
+    install_to = path.join(location, package_name, 'templates')
+
     setup(
         name=package_name,
-        author=__author__,
-        version=__version__,
+        author='Samuel Marks',
+        version='0.3.0',
         test_suite=package_name + '.tests',
         packages=find_packages(),
-        package_dir={package_name: package_name}
+        package_dir={package_name: package_name},
+        data_files=[(install_to, [templates_join('.gitignore'), templates_join('logging.conf')])]
     )
